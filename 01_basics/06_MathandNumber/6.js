6d7aa5181a87da8a9cbac7d6c21f049c604480ad
Raksha Jaiswal <ku.rakshadilipjaiswal@gmail.com>
2024-01-05 15:15:03 +0000

    Math Operation

---
+++ 01_basics/06_MathandNumber/6.js
@@ -0,0 +1,34 @@
+const score = 400;
+
+const balance = new Number(score);  //Number is object here.
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+let num = 100.233;
+console.log(num.toFixed(2));
+
+let num2 = 100.85
+console.log(num2.toPrecision(3));
+
+
+const hundred = 1000000;
+console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+
+//////////////////////////////////////////////////
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(5,4,2,1));
+console.log(Math.random()*100);
+console.log(Math.floor(Math.random()*100));
+
+
+
+
+
